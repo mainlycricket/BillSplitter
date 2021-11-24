98232549e6e4bbe4fbfccf6991e823377a7e2aef
mainlycricket <metusharjain@gmail.com>
2021-11-24 16:53:59 +0530

    Update billsplitter.py
    Added some comments, and making the lucky feature available only if more than one friends join the party!

--- billsplitter.py
+++ billsplitter.py
@@ -1,38 +1,62 @@
-import random
+import random   # Importing the random module
 
-count_friends = int(input("Enter the number of friends joining (including you):\n"))
-friends = {}
-i = 1
+# Reading input from the user to know the number of friends joining the party:
+count_friends = int(input("Enter the number of friends joining (including you):\n> "))
+
+friends = {}    # declaring an empty dictionary
+
+i = 1       # while loop initializer
+
+# checking if at least one member is joining the party:
 
 if count_friends <= 0:
     print("\nNo one is joining for the party")
+
+# in case there is at least one member in the party:
 
 else:
     print("\nEnter the name of every friend (including you), each on a new line:")
 
     while i <= count_friends:
-        friend = input()
+        friend = input("> ")
         friends[friend] = 0
         i += 1
 
-    bill_value = float(input("\nEnter the total bill value:\n"))
+    bill_value = float(input("\nEnter the total bill value:\n"))    # Bill value
 
-    lucky_feature = input('\nDo you want to use the "Who is lucky?" feature? Write Yes/No:\n')
+    # Enable the lucky feature in case there are more than one members
 
-    if lucky_feature == 'Yes':
-        friends_list = list(friends.keys())
-        lucky_friend = random.choice(friends_list)
-        print()
-        print(lucky_friend, "is going to lucky\n")
-        individual_contribution = round(bill_value / (count_friends - 1), 2)
-        for keys, values in friends.items():
-            friends[keys] = individual_contribution
-        friends[lucky_friend] = 0
-        print(friends, end="")
+    if count_friends > 1:
 
+        lucky_feature = input('\nDo you want to use the "Who is lucky?" feature? Write Yes/No:\n')  # User's wish
+
+        # Lucky feature enabled
+
+        if lucky_feature == 'Yes':
+            friends_list = list(friends.keys())
+            lucky_friend = random.choice(friends_list)
+            print()
+            print(lucky_friend, "is going to be lucky\n")
+            individual_contribution = round(bill_value / (count_friends - 1), 2)
+            for keys, values in friends.items():
+                friends[keys] = individual_contribution
+            friends[lucky_friend] = 0
+            print(friends, end="")
+
+        # Lucky feature not enabled:
+
+        else:
+            print("\nNo one is going to be lucky\n")
+            individual_contribution = round(bill_value / count_friends, 2)
+            for keys, values in friends.items():
+                friends[keys] = individual_contribution
+            print(friends, end="")
+
+    # Lucky feature unavailable in case there is only one member
+        
     else:
-        print("\nNo one is going to be lucky\n")
         individual_contribution = round(bill_value / count_friends, 2)
         for keys, values in friends.items():
             friends[keys] = individual_contribution
-        print(friends, end="")
+            print()
+            print(friends, end="")
